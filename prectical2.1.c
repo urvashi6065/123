67bbe50fadc02c86e6bc1574f6eb1eedda84370d
urvashi6065 <138776432+urvashi6065@users.noreply.github.com>
2023-07-07 17:07:40 +0530

    Add files via upload
    operators

---
+++ prectical2.1.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter if");
+    scanf("%i",&a);
+    printf("enter if");
+    scanf("%i",&b);
+    c=a+b;
+    
+  printf("%i\n",c);
+  c=a-b;
+  printf("%i\n",c);
+  
+  c=a*b;
+  printf("%i\n",c);
+  
+  c=a/b;
+  printf("%i",c);
+  
+  c=a%b;
+  printf("%i",c);
+
+    return 0;
+}
