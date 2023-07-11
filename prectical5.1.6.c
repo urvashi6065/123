4a3b000d71584d3418a4016e9d2d11cfa8120da8
urvashi6065 <138776432+urvashi6065@users.noreply.github.com>
2023-07-11 17:18:31 +0530

    Add files via upload

---
+++ prectical5.1.6.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    int ans;
+    
+    printf("ans");
+    scanf("%i",&x);
+    printf("ans");
+    scanf("%i",&y);
+    printf("ans");
+    scanf("%i",&z);
+    
+    ans=(x-y-z)*3;
+    printf("%i",ans);
+    
+    
+
+    return 0;
+}
