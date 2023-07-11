86e8ecdfb08fcc9f8bc4d38835ecfb5dfd2a5230
urvashi6065 <138776432+urvashi6065@users.noreply.github.com>
2023-07-11 17:19:08 +0530

    Add files via upload

---
+++ project.1.c.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+ printf("1.");
+ printf("           *\t   *\n");
+ printf("       *      \t*\n");
+ printf("   *\t     *\n");
+ printf("       *      \t*\n");
+ printf("           *\t   *\n\n\n");
+ 
+printf("2."); 
+printf("        *   *   *   *   *   *   *\n\n");
+printf("    *   *   *   *   *   *   *   *\n");
+printf("    *       *                   *\n");
+printf("    *       *       *   *   *   *\n");
+printf("    *       *       *       *   *\n");
+printf("    *   *   *   *   *   *   *   *");
+
+
+ 
+    return 0;
+}
