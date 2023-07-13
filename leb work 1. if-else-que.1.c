f35a2793ebdf55ea77c6261fc4c55bf303df7eef
urvashi6065 <138776432+urvashi6065@users.noreply.github.com>
2023-07-13 18:11:28 +0530

    Add files via upload

---
+++ leb work 1. if-else-que.1.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int a;
+int b;
+
+printf("condition");
+scanf("%i",&a);
+
+printf("condition");
+scanf("%i",&b);
+
+if(a>b)
+
+{
+    printf("a is max:%i",a);
+    
+}
+    else {
+    printf("b is max:%i",b);
+}
+    return 0;
+}
