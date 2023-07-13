0b9c58cb362f975981a2f52275b18e1c0577ac2a
urvashi6065 <138776432+urvashi6065@users.noreply.github.com>
2023-07-13 14:51:56 +0530

    Add files via upload

---
+++ pettern.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("*  *  *  *          1\n");
+    printf(" *     *          1    1\n");
+    printf("  *   *         1    2    1\n");
+    printf("    *         1   3     3   1\n");
+    printf("  *   *\n");
+    printf(" *     *\n");
+    printf("*  *  *  *\n\n \n");
+    printf("A B C D E F G H I\n");
+    printf("  A B C D E F G\n");
+    printf("    A B C D E\n");
+    printf("      A B C\n");
+    printf("        A \n");
+     printf("     A B C\n");
+     printf("   A B C D E\n");
+     printf(" A B C D E F G\n");
+    printf("A B C D E F G H I\n\n\n");
+    
+    printf("          *\n");
+    printf("        *  *\n");
+    printf("       *    *\n");
+    printf("      *      *\n");
+    printf("    * * * * * * *\n\n\n  ");
+    
+   printf("  *\n");
+   printf("   * *\n");
+   printf("  *   *\n");
+   printf(" *     *\n");
+   printf("*       *\n");
+   printf(" *     *\n");
+   printf("  *   *\n"); 
+    printf("   * *\n"); 
+    printf("    *\n");
+    return 0;
+}
